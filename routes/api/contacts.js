b43aee00d88fd47f05612d65ee868418f9e2ca0e
NatalyaDashkovska <dashkovsk@gmail.com>
2021-06-06 21:55:54 +0300

    start

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,24 +1,114 @@
-const express = require('express')
-const router = express.Router()
+const express = require("express");
+const router = express.Router();
+const Contacts = require("../../model/index");
+const { HttpCode } = require("./helpers/constants");
+const {
+  validateCreateContact,
+  validateUpdateContact,
+} = require("./validation/contact-validation");
 
-router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.get("/", async (_req, res, next) => {
+  try {
+    const contacts = await Contacts.listContacts();
 
-router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+    return res.json({
+      status: "success",
+      code: HttpCode.OK,
+      data: {
+        contacts,
+      },
+    });
+  } catch (error) {
+    next(error);
+  }
+});
 
-router.post('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.get("/:contactId", async (req, res, next) => {
+  try {
+    const contact = await Contacts.getContactById(req.params.contactId);
+    if (contact) {
+      // console.log(contact);
+      return res.json({
+        status: "success",
+        code: HttpCode.OK,
+        data: {
+          contact: contact,
+        },
+      });
+    } else {
+      return next({
+        status: HttpCode.NOT_FOUND,
+        message: "Contact is not found",
+        data: "Not found",
+      });
+    }
+  } catch (e) {
+    next(e);
+  }
+});
 
-router.delete('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.post("/", validateCreateContact, async (req, res, next) => {
+  try {
+    const contact = await Contacts.addContact(req.body);
+    res.status(HttpCode.CREATE).json({
+      status: "success",
+      code: HttpCode.CREATE,
+      data: {
+        contact,
+      },
+    });
+  } catch (e) {
+    next(e);
+  }
+});
 
-router.patch('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.delete("/:contactId", async (req, res, next) => {
+  try {
+    const contact = await Contacts.removeContact(req.params.contactId);
+    if (contact) {
+      return res.status(HttpCode.OK).json({
+        status: "success",
+        code: HttpCode.OK,
+        data: {
+          contact,
+        },
+      });
+    } else {
+      return next({
+        status: HttpCode.NOT_FOUND,
+        message: "Contact is not found",
+        data: "Not found",
+      });
+    }
+  } catch (e) {
+    next(e);
+  }
+});
 
-module.exports = router
+router.patch("/:contactId", validateUpdateContact, async (req, res, next) => {
+  try {
+    const contact = await Contacts.updateContact(
+      req.params.contactId,
+      req.body
+    );
+    if (contact) {
+      return res.status(HttpCode.OK).json({
+        status: "success",
+        code: HttpCode.OK,
+        data: {
+          contact,
+        },
+      });
+    } else {
+      return next({
+        status: HttpCode.NOT_FOUND,
+        message: "Contact is not found",
+        data: "Not found",
+      });
+    }
+  } catch (e) {
+    next(e);
+  }
+});
+
+module.exports = router;
